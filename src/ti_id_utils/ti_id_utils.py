eca12d4e2acdae1e5279e93aca45cc0c2282a25f
Matt Peters <matthew.peters@threatinformant.com>
2024-11-05 13:23:26 +0000

    added docstrings

--- src/ti_id_utils/ti_id_utils.py
+++ src/ti_id_utils/ti_id_utils.py
@@ -2,22 +2,22 @@ import uuid as uuid_lib
 import random
 import string
 
-
-def std_id():
-    # lower case 12 character random string
+def std_id() -> str:
+    """Generates a lowercase 12-character random string."""
     return str(random_string(12).lower())
 
-def short_id():
-    # used for case sensitive ids
+def short_id() -> str:
+    """Generates a case-sensitive 6-character random string."""
     return random_string(6)
 
-def redis_job_id():
-    # random case sensitive string becaus redis keys are case sensitive
+def redis_job_id() -> str:
+    """Generates a case-sensitive 8-character random string for Redis keys."""
     return random_string(8)
 
-def uuid():
-    # generates uuid without dashes
+def uuid() -> str:
+    """Generates a UUID without dashes."""
     return str(uuid_lib.uuid4()).replace("-", "")
 
-def random_string(length: int = 12):
+def random_string(length: int = 12) -> str:
+    """Generates a random string of the given length containing letters and digits."""
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
